d6f644ebd45d62556e7fc1b055ee029b76ce5346
MinexAutomation <minex.automation@gmail.com>
2020-02-06 15:46:23 -0800

    Added IServiceCollection extension.

---
+++ source/R5T.Odense.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.Odense.Default;
+
+
+namespace R5T.Odense.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioSolutionFileProjectTypeGuidProvider"/> service.
+        /// </summary>
+        public static IServiceCollection AddVisualStudioSolutionFileProjectTypeGuidProvider(this IServiceCollection services)
+        {
+            services.AddDefaultVisualStudioSolutionFileProjectTypeGuidProvider();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IVisualStudioSolutionFileProjectTypeGuidProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFileProjectTypeGuidProvider> AddVisualStudioSolutionFileProjectTypeGuidProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFileProjectTypeGuidProvider>(() => services.AddVisualStudioSolutionFileProjectTypeGuidProvider());
+            return serviceAction;
+        }
+    }
+}
